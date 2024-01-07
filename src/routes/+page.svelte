c716a0e36eade8b15ebdafdf6b725404f019df02
Tim Yang <toxicpuzzle@gmail.com>
2024-01-07 18:51:52 +1100

    dot points for ways to hovering in plotly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
     PlotMouseEvent,
     PlotlyHTMLElement,
   } from "plotly.js";
+  import * as d3 from "d3";
   //   import * as Plotly from 'plotly.js'
   import { SVG, defaults, utils } from '@svgdotjs/svg.js'
   import { traces } from "$lib/data/plotdata.js";
@@ -38,39 +39,39 @@
     const pathElement =
       element[0].children[0].getElementsByClassName("point")[0];
 
-	// SVG way of adding scaling transform.
+	// 1. SVG way of adding scaling transform.
 	// Problem is that it cannot change css tags with - inside
-	const svgPoint = SVG(pathElement);
-	svgPoint.stroke({width: 10})
-	svgPoint.css({ fill: "yellow", stroke: "black", strokeWidth: "2px", fillOpacity: "0.5"});
-	svgPoint.scale(1.2);
+	// const svgPoint = SVG(pathElement);
+	// svgPoint.stroke({width: 10})
+	// svgPoint.css({ fill: "yellow", stroke: "black", strokeWidth: "2px", fillOpacity: "0.5"});
+	// svgPoint.scale(1.2);
 
-	svgPoint.fill({color:'green'})
-	svgPoint.stroke({width: 10, color: '#508484'})
+	// svgPoint.fill({color:'green'})
+	// svgPoint.stroke({width: 10, color: '#508484'})
 
-	// One way of hacking so that strokewidth could be set
-	const style = {"stroke-width": "2px", "opacity": '0.5'}
-	svgPoint.css(style);
 
-	// /@ts-expect-error - css selector must be called storkewidth.
+	// 2. d3 way of changing style, no type hints but no jank errors either
+	d3.select(pathElement).style("stroke-width", "5px")
+
+	// 3. // One way of hacking so that strokewidth could be set
+	// const style = {"stroke-width": "2px", "opacity": '0.5'}
+	// svgPoint.css(style);
+
+	// 4. /@ts-expect-error - css selector must be called storkewidth.
 	// svgPoint.css("stroke-width", "3px")
 
 	console.log(pathElement);
 	// Problem is stroke and other attributes are wrapped in style tag so w
 	// cannot easily modify them
 
-	// Plain JS way of mainpulating dom
+	// 5. Plain JS way of mainpulating dom
 	// let transformAttr = pathElement.getAttribute("transform");
     // // Add a scale transformation to it (e.g., scale 2 times)
     // transformAttr += " scale(2)";
     // // Set the modified transform attribute back
     // pathElement.setAttribute("transform", transformAttr!);
-	
 
-	
-
-
-	// Changing traces will cause rerender and invalidate the plot.
+	// 6. Changing traces will cause rerender and invalidate the plot.
     // element[0].children[0].getElementsByClassName("point")[0].setAttribute("d", "M 10,30 A 20,20 0,0,1 50,30 A 20,20 0,0,1 90,30 Q 90,60 50,90 Q 10,60 10,30 z")
     // console.log(data);
     // const point = /\data.detail.points[0];
